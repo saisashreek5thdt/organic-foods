90c9c58bef4b91abae2b9237bdea78ac7adebb1a
Sai Sashreek Dittakavi <53420544+saisashreek5thdt@users.noreply.github.com>
2022-12-28 14:09:54 +0530

    Routing Configuration Changes

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,8 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8080/';
+    //public $baseURL = 'http://localhost:8080/';
+    public $baseURL = 'http://localhost/organicfoods/';
 
     /**
      * --------------------------------------------------------------------------
@@ -56,7 +57,8 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $uriProtocol = 'REQUEST_URI';
+    //public $uriProtocol = 'REQUEST_URI';
+    public $uriProtocol = 'PATH_INFO';
 
     /**
      * --------------------------------------------------------------------------
